02f014ebd253cb72bbd70a7b21d2db74943a20ec
Alex Gorban <gunner14th@gmail.com>
2018-06-22 14:44:42 +0300

    edit taxonomys

--- wp-content/themes/unite-child-theme/functions.php
+++ wp-content/themes/unite-child-theme/functions.php
@@ -16,7 +16,7 @@ function create_post_type() {
     'films',
     array(
       'label' => __( 'Год' ),
-      'rewrite' => array( 'slug' => 'person' )
+      'rewrite' => array( 'slug' => 'year' )
     )
   );
 
@@ -25,7 +25,7 @@ function create_post_type() {
     'films',
     array(
       'label' => __( 'Жанры' ),
-      'rewrite' => array( 'slug' => 'person' )
+      'rewrite' => array( 'slug' => 'genres' )
     )
   );
 
@@ -34,7 +34,7 @@ function create_post_type() {
     'films',
     array(
       'label' => __( 'Страны' ),
-      'rewrite' => array( 'slug' => 'person' )
+      'rewrite' => array( 'slug' => 'countries' )
     )
   );
 
@@ -43,7 +43,7 @@ function create_post_type() {
     'films',
     array(
       'label' => __( 'Актеры' ),
-      'rewrite' => array( 'slug' => 'person' )
+      'rewrite' => array( 'slug' => 'acters' )
     )
   );
 }
